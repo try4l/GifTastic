ad6ecd22fae0b56ce382fa74dbe1dbcfb24e051a
Alan Davison <abdavison@cox.net>
2017-09-13 17:44:14 -0700

    Got gifs. Basic functionality.

--- assets/javascript/giphy-app.js
+++ assets/javascript/giphy-app.js
@@ -2,17 +2,60 @@
       // Initial array of items
       var items = ["Monkeys", "Dogs", "Cats", "Elephants"];
 
-      // Generic function for capturing the item name from the data-attribute
-      $("#buttons-view").on("click", ".item", function (){
+      // displayMovieInfo function re-renders the HTML to display the appropriate content
+      function displayItemInfo() {
+        console.log("displayItemInfo function")
+
         var category = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=7f1c7d7acfc048f88884fb9650342e30&q="+category+"&limit=10&offset=0&rating=G&lang=en";
+        console.log("category: ", category);
+
+        var giphyURL = "https://api.giphy.com/v1/gifs/search?";
+        var apiKey = "api_key=7f1c7d7acfc048f88884fb9650342e30";
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=7f1c7d7acfc048f88884fb9650342e30&q=" 
+          + category 
+          + "&limit=10&offset=0&rating=G&lang=en";
+          console.log("queryURL: ", queryURL);
+
+        // Creating an AJAX call for the specific movie button being clicked
         $.ajax({
-          URL: queryURL,
+          url: queryURL,
           method: "GET"
-        }).done (function(response){
-          console.log("response: ", response);
-        })
-      });
+        }).done(function(response) {
+
+          for (var i = 0; i < response.data.length; i++) {
+
+            console.log("response: ", response);
+            // Creating a div to hold the item
+            var itemDiv = $("<div class='item' + i>");
+
+            var imgURL = response.data[i].images.fixed_width_still.url;
+            console.log("imgURL: ", imgURL);
+
+            // Creating an element to hold the image
+            var image = $("<img>").attr("src", imgURL);
+
+            // Appending the image
+            itemDiv.append(image);
+
+            // Storing the rating data
+            var rating = response.data[i].rating;
+            console.log("rating: ", rating);
+
+            // Creating an element to have the rating displayed
+            var pOne = $("<p>").text("Rating: " + rating);
+
+            // Displaying the rating
+            itemDiv.append(pOne);
+
+            // Putting the items above the previous items
+            $("#items-view").prepend(itemDiv);
+
+          }
+
+        });
+
+      }      
 
       // Function for displaying item data
       function renderButtons() {
@@ -59,8 +102,10 @@
 
       // Function for displaying the item info
       // We're adding a click event listener to all elements with the class "item"
-      // We're adding the event listener to the document because it will work for dynamically generated elements
-      // $(".items").on("click") will only add listeners to elements that are on the page at that time
+      // Note: We're adding the event listener to the document because it will work for dynamically generated elements
+      //          $(".items").on("click") will only add listeners to elements that are on the page at that time
+      $(document).on("click", ".item", displayItemInfo);
+
 
 
       // Calling the renderButtons function to display the intial buttons
