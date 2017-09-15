44178202353e8fa2d05183784c6764533f6fb213
Alan Davison <abdavison@cox.net>
2017-09-15 01:47:33 -0700

    Basic functionality. Needs better styling, layout and refactoring.

--- assets/javascript/giphy-app.js
+++ assets/javascript/giphy-app.js
@@ -7,7 +7,7 @@
         console.log("displayItemInfo function")
 
         var category = $(this).attr("data-name");
-        console.log("category: ", category);
+        //console.log("category: ", category);
 
         var giphyURL = "https://api.giphy.com/v1/gifs/search?";
         var apiKey = "api_key=7f1c7d7acfc048f88884fb9650342e30";
@@ -23,24 +23,37 @@
           method: "GET"
         }).done(function(response) {
 
+          // first clear out any old images
+          $("#items-view").empty();
+
           for (var i = 0; i < response.data.length; i++) {
 
-            console.log("response: ", response);
+            //console.log("response: ", response);
             // Creating a div to hold the item
-            var itemDiv = $("<div class='item' + i>");
+            var itemDiv = $("<div>");
+            //console.log("itemDiv: ", itemDiv);
 
             var imgURL = response.data[i].images.fixed_width_still.url;
-            console.log("imgURL: ", imgURL);
+            var gifURL = response.data[i].images.fixed_width.url;
+            //console.log("imgURL: ", imgURL);
+            //console.log("gifURL: ", gifURL);
 
-            // Creating an element to hold the image
-            var image = $("<img>").attr("src", imgURL);
+
+            // Create an element to hold the image
+            var image = $("<img>")
+              .attr("src", imgURL)
+              .attr("data-animate", gifURL)
+              .attr("data-still", imgURL)
+              .attr("data-state", "still")
+              .addClass("gif");
+            //console.log("image: ", image);
 
             // Appending the image
             itemDiv.append(image);
 
             // Storing the rating data
             var rating = response.data[i].rating;
-            console.log("rating: ", rating);
+            //console.log("rating: ", rating);
 
             // Creating an element to have the rating displayed
             var pOne = $("<p>").text("Rating: " + rating);
@@ -48,14 +61,51 @@
             // Displaying the rating
             itemDiv.append(pOne);
 
-            // Putting the items above the previous items
+            // Putting the items before the previous items
             $("#items-view").prepend(itemDiv);
 
           }
 
         });
 
-      }      
+      } 
+
+      function sayClick () {
+        console.log("Click");
+      }
+
+      // $(".gif").on("click", function() {
+      //   console.log("click");
+      //   // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      //   var URLstate = $(this).attr("data-state");
+      //   // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      //   // Then, set the image's data-state to animate
+      //   // Else set src to the data-still value
+      //   if (state === "still") {
+      //     $(this).attr("src", $(this).attr("data-animate"));
+      //     $(this).attr("data-state", "animate");
+      //   } else {
+      //     $(this).attr("src", $(this).attr("data-still"));
+      //     $(this).attr("data-state", "still");
+      //   }
+      // }); 
+
+      function animateGif () {
+        console.log("click");
+        // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+        var URLstate = $(this).attr("data-state");
+        console.log("URLstate: ", URLstate);
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (URLstate === "still") {
+          $(this).attr("src", $(this).attr("data-animate"));
+          $(this).attr("data-state", "animate");
+        } else {
+          $(this).attr("src", $(this).attr("data-still"));
+          $(this).attr("data-state", "still");
+        }
+      }
 
       // Function for displaying item data
       function renderButtons() {
@@ -86,7 +136,7 @@
 
       // This function handles events where one button is clicked
       $("#add-item").on("click", function(event) {
-        console.log("click");
+        //console.log("click");
         // Preventing the buttons default behavior when clicked (which is submitting a form)
         event.preventDefault();
         // This line grabs the input from the textbox
@@ -106,7 +156,7 @@
       //          $(".items").on("click") will only add listeners to elements that are on the page at that time
       $(document).on("click", ".item", displayItemInfo);
 
-
+      $(document).on("click", ".gif", animateGif);
 
       // Calling the renderButtons function to display the intial buttons
       renderButtons();
